5873e992ab53f59b389dc90d239100aebfb1e4d9
QuiiBz <tom.lienrd@gmail.com>
2022-06-24 21:43:15 +0200

    feat: add confirmation for logout

--- packages/cli/src/commands/logout.ts
+++ packages/cli/src/commands/logout.ts
@@ -1,9 +1,21 @@
+import inquirer from 'inquirer';
 import { deleteAuthFile, isLoggedIn } from '../auth';
 import { logError, logSuccess } from '../utils/logger';
 
 export async function logout() {
   if (!isLoggedIn) {
     logError('You are not logged in.');
+    return;
+  }
+
+  const { confirm } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Are you sure you want to log out?',
+  });
+
+  if (!confirm) {
+    logError(`Aborted logout.`);
     return;
   }
 
