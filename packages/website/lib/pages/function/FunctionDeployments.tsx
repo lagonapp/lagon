4eec586f03574c16298e1d1c5726cea7fba1226e
QuiiBz <tom.lienrd@gmail.com>
2022-06-24 16:38:20 +0200

    feat: add confirmation modal on deployments

--- packages/website/lib/pages/function/FunctionDeployments.tsx
+++ packages/website/lib/pages/function/FunctionDeployments.tsx
@@ -9,6 +9,7 @@ import EmptyState from 'lib/components/EmptyState';
 import Link from 'lib/components/Link';
 import Text from 'lib/components/Text';
 import { getCurrentDomain, getFullCurrentDomain } from 'lib/utils';
+import Dialog from 'lib/components/Dialog';
 
 type FunctionDeploymentsProps = {
   func: GetFunctionResponse;
@@ -82,7 +83,18 @@ const FunctionDeployments = ({ func }: FunctionDeploymentsProps) => {
             </Text>
             {!deployment.isCurrent ? (
               <>
-                <Button onClick={() => deleteDeployment(deployment)}>Delete</Button>
+                <Dialog
+                  title="Delete Deployment"
+                  description="Are you sure you want to delete this Deployment?"
+                  disclosure={<Button>Delete</Button>}
+                >
+                  <Dialog.Buttons>
+                    <Dialog.Cancel />
+                    <Dialog.Action variant="danger" onClick={() => deleteDeployment(deployment)}>
+                      Delete
+                    </Dialog.Action>
+                  </Dialog.Buttons>
+                </Dialog>
                 <Button onClick={() => rollbackDeployment(deployment)}>Rollback</Button>
               </>
             ) : null}
