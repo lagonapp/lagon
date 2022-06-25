03e236ac0a33abf7ca2be72ef7f466b6a131b54e
QuiiBz <tom.lienrd@gmail.com>
2022-06-25 17:37:09 +0200

    feat(www): add landing page

---
+++ www/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -14,7 +14,22 @@ const Home = () => {
           <link rel="icon" href="/favicon-black.ico" />
         )}
       </Head>
-      <p>Lagon allows you to write JavaScript Serverless Functions and deploy them at the Edge, with no cold start.</p>
+      <div className="flex flex-col gap-8 items-center justify-center w-screen h-screen text-center">
+        <h1 className="text-white text-5xl font-bold leading-[42px]" style={{ fontFamily: 'Poppins' }}>
+          Deploy Serverless <br /> Functions at the{' '}
+          <span className="bg-clip-text text-transparent bg-gradient-to-br from-blue-500 to-purple-500">Edge</span>
+        </h1>
+        <h2 className="text-gray-300 text-sm">
+          Lagon is an open source platform that allows you to run <br /> TypeScript and JavaScript close to your users.
+        </h2>
+      </div>
+      <button
+        type="button"
+        className="absolute bottom-[20vh] left-[50vw] transform -translate-x-[50%] z-10 text- text-purple-100 px-8 py-2 rounded-full bg-gradient-to-br from-purple-500/70 to-purple-500/0 border border-purple-500"
+      >
+        Deploy now
+      </button>
+      <img src="/world.svg" alt="Image of the world" className="absolute -bottom-[20vh] w-[130vw]" />
     </>
   );
 };
