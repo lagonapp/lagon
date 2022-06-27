6e42ca4a5248defef7a4d36f1313214cca2ddca5
QuiiBz <tom.lienrd@gmail.com>
2022-06-27 13:44:28 +0200

    refactor: convert missing token authentication mutation

--- packages/website/lib/trpc/tokensRouter.ts
+++ packages/website/lib/trpc/tokensRouter.ts
@@ -1,35 +1,88 @@
 import prisma from 'lib/prisma';
 import { createRouter } from 'pages/api/trpc/[trpc]';
+import { z } from 'zod';
 
 export const tokensRouter = () =>
-  createRouter().query('verification-code', {
-    resolve: async ({ ctx }) => {
-      const user = await prisma.user.findFirst({
-        where: {
-          id: ctx.session.user.id,
-        },
-        select: {
-          id: true,
-          verificationCode: true,
-        },
-      });
+  createRouter()
+    .query('verification-code', {
+      resolve: async ({ ctx }) => {
+        const user = await prisma.user.findFirst({
+          where: {
+            id: ctx.session.user.id,
+          },
+          select: {
+            id: true,
+            verificationCode: true,
+          },
+        });
 
-      let verificationCode: string | null = user.verificationCode;
+        let verificationCode: string | null = user.verificationCode;
 
-      if (!verificationCode) {
-        verificationCode = (
-          await prisma.user.update({
-            where: {
-              id: user.id,
-            },
+        if (!verificationCode) {
+          verificationCode = (
+            await prisma.user.update({
+              where: {
+                id: user.id,
+              },
+              data: {
+                verificationCode:
+                  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+              },
+            })
+          ).verificationCode;
+        }
+
+        return { code: verificationCode };
+      },
+    })
+    .mutation('authenticate', {
+      input: z.object({
+        code: z.string(),
+      }),
+      resolve: async ({ input }) => {
+        const user = await prisma.user.findFirst({
+          where: {
+            verificationCode: input.code,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!user) {
+          return { error: 'Invalid verification code' };
+        }
+
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            verificationCode: null,
+          },
+        });
+
+        let token = await prisma.token.findFirst({
+          where: {
+            userId: user.id,
+          },
+          select: {
+            value: true,
+          },
+        });
+
+        if (!token) {
+          token = await prisma.token.create({
             data: {
-              verificationCode:
-                Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+              value: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+              userId: user.id,
             },
-          })
-        ).verificationCode;
-      }
+            select: {
+              value: true,
+            },
+          });
+        }
 
-      return { code: verificationCode };
-    },
-  });
+        return { token: token.value };
+      },
+    });
