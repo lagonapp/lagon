bdc632cd03c47bd4a14e90328175d9a79d82c881
QuiiBz <tom.lienrd@gmail.com>
2022-06-28 09:55:44 +0200

    fix(Button): forward ref

--- packages/website/lib/components/Button/index.tsx
+++ packages/website/lib/components/Button/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/display-name */
 import Link from 'next/link';
-import { MouseEventHandler, ReactElement, ReactNode } from 'react';
+import { forwardRef, MouseEventHandler, MutableRefObject, ReactElement, ReactNode } from 'react';
 import useTailwind from 'lib/hooks/useTailwind';
 
 type ButtonProps = {
@@ -15,70 +16,77 @@ type ButtonProps = {
   children: ReactNode;
 };
 
-const Button = ({
-  variant = 'secondary',
-  size = 'md',
-  disabled,
-  leftIcon,
-  rightIcon,
-  center,
-  submit,
-  href,
-  onClick,
-  children,
-}: ButtonProps) => {
-  const styles = useTailwind(
+const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+  (
     {
-      variant,
-      size,
+      variant = 'secondary',
+      size = 'md',
       disabled,
+      leftIcon,
+      rightIcon,
       center,
-    },
-    {
-      variant: {
-        primary: 'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white border border-blue-700 shadow-blue-100',
-        secondary:
-          'bg-stone-50 hover:bg-stone-100 active:bg-stone-200 text-stone-800 border border-stone-300 shadow-stone-100',
-        danger: 'bg-red-500 hover:bg-red-600 active:bg-red-700 text-white border border-red-700 shadow-red-100',
+      submit,
+      href,
+      onClick,
+      children,
+    }: ButtonProps,
+    ref,
+  ) => {
+    const styles = useTailwind(
+      {
+        variant,
+        size,
+        disabled,
+        center,
       },
-      size: {
-        sm: 'px-2 py-1 rounded text-xs',
-        md: 'px-3 py-1 rounded-md text-sm',
-        lg: 'px-4 py-2 rounded-lg text-md',
+      {
+        variant: {
+          primary: 'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white border border-blue-700 shadow-blue-100',
+          secondary:
+            'bg-stone-50 hover:bg-stone-100 active:bg-stone-200 text-stone-800 border border-stone-300 shadow-stone-100',
+          danger: 'bg-red-500 hover:bg-red-600 active:bg-red-700 text-white border border-red-700 shadow-red-100',
+        },
+        size: {
+          sm: 'px-2 py-1 rounded text-xs',
+          md: 'px-3 py-1 rounded-md text-sm',
+          lg: 'px-4 py-2 rounded-lg text-md',
+        },
+        disabled: 'cursor-not-allowed opacity-50',
+        center: 'justify-center',
       },
-      disabled: 'cursor-not-allowed opacity-50',
-      center: 'justify-center',
-    },
-  );
-
-  if (href) {
-    return (
-      <Link href={href}>
-        <a
-          aria-disabled={disabled}
-          className={`${styles} select-none flex gap-2 items-center whitespace-nowrap shadow-sm`}
-        >
-          {leftIcon}
-          {children}
-          {rightIcon}
-        </a>
-      </Link>
     );
-  }
 
-  return (
-    <button
-      onClick={!disabled ? onClick : undefined}
-      type={submit ? 'submit' : 'button'}
-      disabled={disabled}
-      aria-disabled={disabled}
-      className={`${styles} transition select-none flex gap-2 items-center whitespace-nowrap shadow-sm`}
-    >
-      {leftIcon}
-      {children}
-      {rightIcon}
-    </button>
-  );
-};
+    if (href) {
+      return (
+        <Link href={href}>
+          <a
+            ref={ref as MutableRefObject<HTMLAnchorElement>}
+            aria-disabled={disabled}
+            className={`${styles} select-none flex gap-2 items-center whitespace-nowrap shadow-sm`}
+          >
+            {leftIcon}
+            {children}
+            {rightIcon}
+          </a>
+        </Link>
+      );
+    }
+
+    return (
+      <button
+        ref={ref as MutableRefObject<HTMLButtonElement>}
+        onClick={!disabled ? onClick : undefined}
+        type={submit ? 'submit' : 'button'}
+        disabled={disabled}
+        aria-disabled={disabled}
+        className={`${styles} transition select-none flex gap-2 items-center whitespace-nowrap shadow-sm`}
+      >
+        {leftIcon}
+        {children}
+        {rightIcon}
+      </button>
+    );
+  },
+);
 
 export default Button;
