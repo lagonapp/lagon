fce217e92d0eacb4b6f6bf901b31a8bcc5c34487
QuiiBz <tom.lienrd@gmail.com>
2022-06-28 11:10:28 +0200

    fix: missing ,

--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         fadeTranslate: {
           '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
           '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
-        }
+        },
       }),
     },
   },
