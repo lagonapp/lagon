e8d656c5e33510ee7f2950473c154c0d1acde75a
await-ovo <13152410380@163.com>
2022-07-12 10:57:51 +0800

    chore: use mrmime to lookup mime types
    chore: change public asset fallback type to text/plain

--- packages/common/src/helpers/extensions.ts
+++ packages/common/src/helpers/extensions.ts
@@ -1,14 +1,3 @@
-export const extensionToContentType = {
-  '.js': 'application/javascript',
-  '.css': 'text/css',
-  '.html': 'text/html',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.jpeg': 'image/jpeg',
-  '.gif': 'image/gif',
-  '.svg': 'image/svg+xml',
-  '.woff': 'application/font-woff',
-  '.woff2': 'application/font-woff2',
-  '.ttf': 'application/font-ttf',
-  '.otf': 'application/font-otf',
-};
+import { lookup } from 'mrmime';
+
+export const extensionToContentType = lookup;
