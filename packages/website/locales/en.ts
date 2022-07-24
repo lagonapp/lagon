7bb72e0ed9a67376b3d1c5e95d72093aadf44d86
QuiiBz <tom.lienrd@gmail.com>
2022-07-24 11:35:45 +0200

    feat(website): add translation to settings

--- packages/website/locales/en.ts
+++ packages/website/locales/en.ts
@@ -24,4 +24,29 @@ export default {
   'settings.delete.modal.description': "Write this Organization's name to confirm deletion: {organizationName}",
   'settings.delete.modal.error': 'Confirm with the name of this Funtion',
   'settings.delete.modal.submit': 'Delete Organization',
+
+  'profile.information.title': 'Information',
+  'profile.information.description': 'Edit your account information like your name and email.',
+  'profile.information.name.title': 'Name',
+  'profile.information.name.placeholder': 'John Doe',
+  'profile.information.email.title': 'Email',
+  'profile.information.email.placeholder': 'john@doe.com',
+  'profile.information.submit': 'Update',
+
+  'profile.tokens.title': 'Tokens',
+  'profile.tokens.description': 'Below are your personal tokens, used for the CLI.',
+  'profile.tokens.created': 'Created:',
+  'profile.tokens.delete.submit': 'Delete',
+  'profile.tokens.delete.modal.title': 'Delete Token',
+  'profile.tokens.delete.modal.description':
+    'Are you sure you want to delete this token? You will lose access to the CLI if it is still used.',
+  'profile.tokens.delete.modal.submit': 'Delete Token',
+
+  'profile.delete.title': 'Delete',
+  'profile.delete.description': 'Delete permanentently this account. This action is irreversible.',
+  'profile.delete.submit': 'Delete',
+  'profile.delete.modal.title': 'Delete Account',
+  'profile.delete.modal.description': 'Write your account email to confirm deletion: {email}',
+  'profile.delete.modal.confirm': 'Confirm with your email',
+  'profile.delete.modal.submit': 'Delete Account',
 } as const;
