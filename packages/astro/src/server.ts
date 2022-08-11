217cef716df2fab3bb97b7362a8feb3501d07c68
QuiiBz <tom.lienrd@gmail.com>
2022-08-11 19:48:43 +0200

    fix(astro): let runtime handle assets

--- packages/astro/src/server.ts
+++ packages/astro/src/server.ts
@@ -1,20 +1,12 @@
-// Normal Imports
 import type { SSRManifest } from 'astro';
 import { App } from 'astro/app';
 
 export function createExports(manifest: SSRManifest) {
   const app = new App(manifest, false);
 
-  const handler = async (request: Request, env: Env) => {
-    const { origin, pathname } = new URL(request.url);
-
-    // static assets
-    if (manifest.assets.has(pathname)) {
-      const assetRequest = new Request(`${origin}/static${pathname}`, request);
-      return env.ASSETS.fetch(assetRequest);
-    }
-
+  const handler = async (request: Request) => {
     const routeData = app.match(request, { matchNotFound: true });
+
     if (routeData) {
       Reflect.set(request, Symbol.for('astro.clientAddress'), request.headers.get('cf-connecting-ip'));
       return app.render(request, routeData);
