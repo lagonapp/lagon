a5f4cd0581db3a0fdfa62b469f76148827b977ef
QuiiBz <tom.lienrd@gmail.com>
2022-09-20 08:12:29 +0200

    feat: add function bundling

--- packages/rust-cli/src/commands/build.rs
+++ packages/rust-cli/src/commands/build.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use crate::utils::{
     bundle_function, validate_client_file, validate_index_file, validate_public_dir,
@@ -18,5 +18,22 @@ pub fn build(file: PathBuf, client: Option<PathBuf>, public_dir: Option<PathBuf>
 
     let public_dir = public_dir.unwrap();
 
-    bundle_function(file, client, public_dir);
+    if let Some((index, assets)) = bundle_function(file, client, public_dir) {
+        println!();
+        println!("Writting index.js...");
+
+        fs::create_dir_all(".lagon").unwrap();
+        fs::write(".lagon/index.js", index).unwrap();
+
+        for (path, content) in assets {
+            println!("Writting {}...", path);
+
+            fs::create_dir_all(format!(".lagon/{}", path)).unwrap();
+            fs::write(format!(".lagon/{}", path), content).unwrap();
+        }
+
+        println!();
+        println!("Build successful! You can find it in .lagon");
+        println!();
+    }
 }
--- packages/rust-cli/src/utils/deployments.rs
+++ packages/rust-cli/src/utils/deployments.rs
@@ -1,3 +1,71 @@
-use std::path::PathBuf;
+use std::{
+    collections::HashMap,
+    io::{self, Error, ErrorKind},
+    path::PathBuf,
+    process::Command,
+};
 
-pub fn bundle_function(index: PathBuf, client: Option<PathBuf>, public_dir: PathBuf) {}
+fn esbuild(file: &PathBuf) -> io::Result<String> {
+    let result = Command::new("esbuild")
+        .arg(file)
+        .arg("--bundle")
+        .arg("--format=esm")
+        .arg("--target=es2020")
+        .arg("--platform=browser")
+        .output();
+
+    if let Err(e) = result {
+        return Err(Error::new(
+            ErrorKind::Other,
+            format!("Failed to bundle function: {}", e),
+        ));
+    }
+
+    // TODO: check status code
+    let output = result.unwrap().stdout;
+
+    match String::from_utf8(output) {
+        Ok(s) => Ok(s),
+        Err(_) => Err(Error::new(
+            ErrorKind::Other,
+            "Failed to convert output to string",
+        )),
+    }
+}
+
+pub fn bundle_function(
+    index: PathBuf,
+    client: Option<PathBuf>,
+    public_dir: PathBuf,
+) -> Option<(String, HashMap<String, String>)> {
+    if let Err(_) = Command::new("esbuild").arg("--version").output() {
+        println!("esbuild is not installed. Please install it with `npm install -g esbuild`");
+        return None;
+    }
+
+    let index_output = esbuild(&index);
+
+    if let Err(e) = index_output {
+        println!("{}", e);
+        return None;
+    }
+
+    let index_output = index_output.unwrap();
+    let mut assets = HashMap::<String, String>::new();
+
+    if let Some(client) = client {
+        let client_output = esbuild(&client);
+
+        if let Err(e) = client_output {
+            println!("{}", e);
+            return None;
+        }
+
+        assets.insert(
+            client.into_os_string().into_string().unwrap(),
+            client_output.unwrap(),
+        );
+    }
+
+    Some((index_output, assets))
+}
