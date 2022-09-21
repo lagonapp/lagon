848e7a53859f35ccdb6df31a56c182fd6c448971
Tom Lienard <tom.lienrd@gmail.com>
2022-09-21 19:42:46 +0200

    refactor(website): replace splitbee by posthog (#144)

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,5 +1,11 @@
 import { AppProps } from 'next/app';
+import posthog from 'posthog-js';
 import '../styles/globals.css';
+
+// Only init PostHog on client-side and production env
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+  posthog.init('phc_PfiPF8GAEzXURYWu9VaNI4lm6wNHReHMQ93OQ1iHuZx', { api_host: 'https://app.posthog.com' });
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
