e7098649f204c8573df5de5fbcc72a07ad01d367
QuiiBz <tom.lienrd@gmail.com>
2022-09-25 08:52:17 +0200

    fix: add debug ls

--- packages/cli/npm/getBinary.js
+++ packages/cli/npm/getBinary.js
@@ -5,11 +5,10 @@ function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === 'Windows_NT' && arch === 'x64') return 'win-x64';
+  if (type === 'Windows_NT') return 'win-x64';
   if (type === 'Linux' && arch === 'x64') return 'linux-x64';
   if (type === 'Linux' && arch === 'arm64') return 'linux-arm64';
-  if (type === 'Darwin' && arch === 'x64') return 'darwin-x64';
-  if (type === 'Darwin' && arch === 'arm64') return 'darwin-arm64';
+  if (type === 'Darwin') return 'darwin-x64';
 
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
@@ -20,7 +19,8 @@ function getBinary() {
 
   const platform = getPlatform();
   const { version } = require('../package.json');
-  const url = `https://github.com/lagonapp/lagon/releases/download/v${version}/lagon-${platform}.tar.gz`;
+  // %40lagon%2Fcli%40 translates to @lagon/cli@
+  const url = `https://github.com/lagonapp/lagon/releases/download/%40lagon%2Fcli%40${version}/lagon-${platform}.tar.gz`;
   const name = 'lagon';
   return new Binary(name, url);
 }
