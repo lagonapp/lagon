bfa85c7dbbdd3a94cb75f53ac16f5be0efd9c1b2
Tom Lienard <tom.lienrd@gmail.com>
2022-10-19 08:07:06 +0200

    fix(cli): binary name on Windows & correctly install (#192)

--- packages/cli/npm/getBinary.js
+++ packages/cli/npm/getBinary.js
@@ -5,23 +5,43 @@ function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === 'Windows_NT') return 'win-x64';
-  if (type === 'Linux' && arch === 'x64') return 'linux-x64';
-  if (type === 'Linux' && arch === 'arm64') return 'linux-arm64';
-  if (type === 'Darwin') return 'darwin-x64';
+  if (type === 'Windows_NT') {
+    return {
+      platform: 'lagon-win-x64',
+      name: 'lagon.exe',
+    };
+  }
+
+  if (type === 'Linux' && arch === 'x64') {
+    return {
+      platform: 'lagon-linux-x64',
+      name: 'lagon',
+    };
+  }
+
+  if (type === 'Linux' && arch === 'arm64') {
+    return {
+      platform: 'lagon-linux-arm64',
+      name: 'lagon',
+    };
+  }
+
+  if (type === 'Darwin') {
+    return {
+      platform: 'lagon-darwin-x64',
+      name: 'lagon',
+    };
+  }
 
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
 
 function getBinary() {
-  // Prevent exiting with code 1
-  process.exit = () => {};
+  const { platform, name } = getPlatform();
+  const { name: packageName, version } = require('../package.json');
 
-  const platform = getPlatform();
-  const { version } = require('../package.json');
-  // %40lagon%2Fcli%40 translates to @lagon/cli@
-  const url = `https://github.com/lagonapp/lagon/releases/download/%40lagon%2Fcli%40${version}/lagon-${platform}.tar.gz`;
-  const name = 'lagon';
+  const url = `https://github.com/lagonapp/lagon/releases/download/${packageName}@${version}/${platform}.tar.gz`;
+
   return new Binary(name, url);
 }
 
