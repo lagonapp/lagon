0b67add5f1fe0df5d4dd368d4ea38168d2b9b371
QuiiBz <tom.lienrd@gmail.com>
2022-10-25 22:12:21 +0200

    test(runtime): add tests for streaming

---
+++ packages/runtime/tests/streams.rs
@@ -0,0 +1,143 @@
+use std::{collections::HashMap, sync::Once};
+
+use hyper::body::Bytes;
+use lagon_runtime::{
+    http::{Request, Response, RunResult, StreamResult},
+    isolate::{Isolate, IsolateOptions},
+    runtime::{Runtime, RuntimeOptions},
+};
+
+fn setup() {
+    static START: Once = Once::new();
+
+    START.call_once(|| {
+        Runtime::new(RuntimeOptions::default());
+    });
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn sync_streaming() {
+    setup();
+    let mut isolate = Isolate::new(IsolateOptions::new(
+        "export function handler() {
+    return new Response(
+        new ReadableStream({
+            pull(controller) {
+                controller.enqueue(new Uint8Array([65, 66, 67]));
+                controller.close();
+            },
+        }),
+    );
+}"
+        .into(),
+    ));
+    let (tx, rx) = flume::unbounded();
+    isolate.run(Request::default(), tx).await;
+
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Data(vec![65, 66, 67]))
+    );
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Done)
+    );
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Start(Response::from(
+            "[object ReadableStream]"
+        )))
+    );
+    assert!(rx.recv_async().await.is_err());
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn sync_streaming_multiple() {
+    setup();
+    let mut isolate = Isolate::new(IsolateOptions::new(
+        "export function handler() {
+    let count = 0;
+    return new Response(
+        new ReadableStream({
+            pull(controller) {
+                count++;
+
+                controller.enqueue(new Uint8Array([65]));
+
+                if (count == 3) {
+                    controller.close();
+                }
+            },
+        }),
+    );
+}"
+        .into(),
+    ));
+    let (tx, rx) = flume::unbounded();
+    isolate.run(Request::default(), tx).await;
+
+    for _ in 0..3 {
+        assert_eq!(
+            rx.recv_async().await.unwrap(),
+            RunResult::Stream(StreamResult::Data(vec![65]))
+        );
+    }
+
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Done)
+    );
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Start(Response::from(
+            "[object ReadableStream]"
+        )))
+    );
+    assert!(rx.recv_async().await.is_err());
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn streaming_with_correct_response() {
+    setup();
+    let mut isolate = Isolate::new(IsolateOptions::new(
+        "export function handler() {
+    return new Response(
+        new ReadableStream({
+            pull(controller) {
+                controller.enqueue(new Uint8Array([65, 66, 67]));
+                controller.close();
+            },
+        }),
+        {
+            status: 201,
+            headers: {
+                'x-lagon': 'test',
+            },
+        },
+    );
+}"
+        .into(),
+    ));
+    let (tx, rx) = flume::unbounded();
+    isolate.run(Request::default(), tx).await;
+
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Data(vec![65, 66, 67]))
+    );
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Done)
+    );
+    let mut headers = HashMap::new();
+    headers.insert("x-lagon".into(), "test".into());
+    assert_eq!(
+        rx.recv_async().await.unwrap(),
+        RunResult::Stream(StreamResult::Start(Response {
+            body: Bytes::from("[object ReadableStream]"),
+            status: 201,
+            headers: Some(headers),
+        }))
+    );
+    assert!(rx.recv_async().await.is_err());
+}
