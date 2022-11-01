dd3733a7f82672e00739f7f303040a8879473ed1
QuiiBz <tom.lienrd@gmail.com>
2022-11-01 09:42:43 +0100

    feat(js-runtime): implement basic CryptoSubtle methods

--- packages/js-runtime/src/runtime/global/crypto.ts
+++ packages/js-runtime/src/runtime/global/crypto.ts
@@ -15,7 +15,132 @@ const getRandomValues = <
 
 const randomUUID = () => Lagon.uuid();
 
-class CryptoSubtle {}
+type EncryptDecryptoAlgorithm =
+  | {
+      name: 'RSA-OAEP';
+      label?: ArrayBuffer;
+    }
+  | {
+      name: 'AES-CBC' | 'AES-CGM';
+      iv: ArrayBuffer;
+    }
+  | {
+      name: 'AES-CTR';
+      counter: ArrayBuffer;
+      length: number;
+    };
+
+type GenAlgorithm =
+  | {
+      name: 'HMAC';
+      hash: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512';
+      length?: number;
+    }
+  | {
+      name: 'ECDSA' | 'ECDH';
+      namedCurve: 'P-256' | 'P-384' | 'P-521';
+    }
+  | {
+      name: 'RSASSA-PKCS1-v1_5' | 'RSA-PSS' | 'RSA-OAEP';
+      modulusLength: number;
+      publicExponent: Uint8Array;
+      hash: 'SHA-256' | 'SHA-384' | 'SHA-512';
+    }
+  | {
+      name: 'AES-CBC' | 'AES-CTR' | 'AES-GCM' | 'AES-KW';
+      length: number;
+    };
+
+type SignVerifyAlgorithm =
+  | 'RSASSA-PKCS1-v1_5'
+  | {
+      name: 'RSASSA-PKCS1-v1_5';
+    }
+  | {
+      name: 'RSA-PSS';
+      saltLength: number;
+    }
+  | {
+      name: 'ECDSA';
+      hash: 'SHA-256' | 'SHA-384' | 'SHA-512';
+    }
+  | 'HMAC'
+  | {
+      name: 'HMAC';
+    };
+
+type AlgorithmName = GenAlgorithm['name'];
+
+type KeyUsage = 'encrypt' | 'decrypt' | 'sign' | 'verify' | 'deriveKey' | 'deriveBits' | 'wrapKey' | 'unwrapKey';
+type CryptoKeyType = 'secret' | 'private' | 'public';
+
+const SYMMETRIC_ALGORITHMS: AlgorithmName[] = ['HMAC', 'AES-CBC', 'AES-CTR', 'AES-GCM', 'AES-KW'];
+
+class CryptoKey {
+  type: CryptoKeyType;
+  extractable: boolean;
+  algorithm: GenAlgorithm;
+  usages: KeyUsage[];
+
+  constructor(type: CryptoKeyType, extractable: boolean, algorithm: GenAlgorithm, usages: KeyUsage[]) {
+    this.type = type;
+    this.extractable = extractable;
+    this.algorithm = algorithm;
+    this.usages = usages;
+  }
+}
+
+class CryptoKeyPair {
+  privateKey: CryptoKey;
+  publicKey: CryptoKey;
+
+  constructor(privateKey: CryptoKey, publicKey: CryptoKey) {
+    this.privateKey = privateKey;
+    this.publicKey = publicKey;
+  }
+}
+
+class CryptoSubtle {
+  async encrypt(algorithm: EncryptDecryptoAlgorithm, key: CryptoKey, data: ArrayBuffer): Promise<ArrayBuffer> {
+    return data;
+  }
+
+  async decrypt(algorithm: EncryptDecryptoAlgorithm, key: CryptoKey, data: ArrayBuffer): Promise<ArrayBuffer> {
+    return data;
+  }
+
+  async sign(algorithm: SignVerifyAlgorithm, key: CryptoKey, data: ArrayBuffer): Promise<ArrayBuffer> {
+    return data;
+  }
+
+  async verify(
+    algorithm: SignVerifyAlgorithm,
+    key: CryptoKey,
+    signature: ArrayBuffer,
+    data: ArrayBuffer,
+  ): Promise<boolean> {
+    return false;
+  }
+
+  async digest(algorithm: 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512', data: ArrayBuffer): Promise<ArrayBuffer> {
+    return data;
+  }
+
+  async generateKey(
+    algorithm: GenAlgorithm,
+    extractable: boolean,
+    keyUsages: KeyUsage[],
+  ): Promise<CryptoKey | CryptoKeyPair> {
+    if (SYMMETRIC_ALGORITHMS.includes(algorithm.name)) {
+      return new CryptoKey('secret', extractable, algorithm, keyUsages);
+    } else {
+      return new CryptoKeyPair(
+        new CryptoKey('private', extractable, algorithm, keyUsages),
+        new CryptoKey('public', extractable, algorithm, keyUsages),
+      );
+    }
+  }
+}
 
 export const crypto = {
   getRandomValues,
