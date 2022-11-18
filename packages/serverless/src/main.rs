40f5f0482306b372a9c3e4733f63a6f6c739bb69
QuiiBz <tom.lienrd@gmail.com>
2022-11-18 09:09:50 +0100

    fix: uncomment

--- packages/serverless/src/main.rs
+++ packages/serverless/src/main.rs
@@ -280,10 +280,10 @@ async fn main() -> Result<()> {
     let url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let url = url.as_str();
     let opts = Opts::from_url(url).expect("Failed to parse DATABASE_URL");
-    // #[cfg(not(debug_assertions))]
-    // let opts = OptsBuilder::from_opts(opts).ssl_opts(Some(
-    //     SslOpts::default().with_danger_accept_invalid_certs(true),
-    // ));
+    #[cfg(not(debug_assertions))]
+    let opts = OptsBuilder::from_opts(opts).ssl_opts(Some(
+        SslOpts::default().with_danger_accept_invalid_certs(true),
+    ));
     let pool = Pool::new(opts)?;
     let conn = pool.get_conn()?;
 
