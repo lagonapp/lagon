f4999c698d5e50da4b1b315bb5f6645df5f65698
QuiiBz <tom.lienrd@gmail.com>
2022-11-19 12:01:07 +0100

    refactor: remove old page

--- www/lib/components/Header.tsx
+++ www/lib/components/Header.tsx
@@ -1,55 +1,29 @@
+import { Button } from './Button';
+import { Text } from './Text';
 import Image from 'next/image';
-import Link from 'next/link';
-import { HTMLAttributeAnchorTarget, ReactNode } from 'react';
-import { DiscordIcon, GitHubIcon } from './Icons';
 
-type HeaderLinkProps = {
-  href: string;
-  target?: HTMLAttributeAnchorTarget;
-  soon?: boolean;
-  children: ReactNode;
-};
-
-const HeaderLink = ({ href, target, soon, children }: HeaderLinkProps) => {
+export const Header = () => {
   return (
-    <a
-      href={href}
-      target={target}
-      className="text-sm text-gray-300 hover:text-white flex gap-1 items-center select-none transition"
-    >
-      {children}
-      {soon ? <span className="text-xs text-gray-400">soon</span> : null}
-    </a>
-  );
-};
-
-const Header = () => {
-  return (
-    <header className="flex items-center justify-between max-w-4xl mx-auto mt-6 px-6 flex-col gap-2 md:flex-row md:gap-0">
-      <Link href="/">
-        <Image width="105" height="32" className="h-8" src="/logo-white.png" alt="Lagon logo" />
-      </Link>
-      <div className="flex gap-8">
-        <HeaderLink href="#" soon>
+    <header className="container sticky top-0 mx-auto flex items-center justify-between bg-dark/50 backdrop-blur py-4 px-8 rounded-full z-10">
+      <Image src="/logo-small-white.png" width="80" height="24" alt="Lagon Logo" />
+      <div className="flex gap-12">
+        <Text size="a" href="#">
           Features
-        </HeaderLink>
-        <HeaderLink href="#" soon>
+        </Text>
+        <Text size="a" href="#">
+          Docs
+        </Text>
+        <Text size="a" href="#">
           Pricing
-        </HeaderLink>
-        <HeaderLink href="https://docs.lagon.app">Documentation</HeaderLink>
+        </Text>
+        <Text size="a" href="#">
+          Community
+        </Text>
       </div>
-      <div className="flex gap-8">
-        <HeaderLink href="https://discord.lagon.app" target="_blank">
-          <DiscordIcon />
-          Discord
-        </HeaderLink>
-        <HeaderLink href="https://github.com/lagonapp/lagon" target="_blank">
-          <GitHubIcon />
-          GitHub
-        </HeaderLink>
+      <div className="flex gap-4">
+        <Button variant="tertiary">Get started</Button>
+        <Button variant="secondary">Sign in</Button>
       </div>
     </header>
   );
 };
-
-export default Header;
