89f345b97fbd714de62ab70d13c6e9ee5880dd1e
QuiiBz <tom.lienrd@gmail.com>
2022-11-19 11:50:16 +0100

    fix: card gradient

--- www/lib/components/new/Card.tsx
+++ www/lib/components/new/Card.tsx
@@ -38,7 +38,7 @@ export const Card = ({ className, lineAnimation, children }: CardProps) => {
   return (
     <div
       className="p-[1px] rounded-2xl relative"
-      style={{ backgroundImage: 'linear-gradient(to bottom right, rgba(255, 255, 255, 0.2), transparent)' }}
+      style={{ backgroundImage: 'linear-gradient(to bottom, rgba(255, 255, 255, 0.2), transparent)' }}
     >
       <div
         className={`h-full ${className}`}
