ddbf57922c01f3bc17a7c3cf2cce6d0ca6b12db6
QuiiBz <tom.lienrd@gmail.com>
2022-11-28 09:00:23 +0100

    refactor(dashboard): log tRPC errors to console + clean code

--- packages/dashboard/lib/trpc.ts
+++ packages/dashboard/lib/trpc.ts
@@ -2,29 +2,24 @@ import { createTRPCNext } from '@trpc/next';
 import { httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../pages/api/trpc/[trpc]';
 
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export const trpc = createTRPCNext<AppRouter>({
   config: () => {
-    if (typeof window !== 'undefined') {
-      return {
-        links: [
-          httpBatchLink({
-            url: '/api/trpc',
-          }),
-        ],
-        queryClientConfig: {
-          defaultOptions: {
-            queries: {
-              suspense: true,
-            },
-          },
-        },
-      };
-    }
-
     return {
       links: [
         httpBatchLink({
-          url: process.env.NEXTAUTH_URL!,
+          url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
       queryClientConfig: {
