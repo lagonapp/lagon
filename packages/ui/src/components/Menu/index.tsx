cdc8edf2334a6c652208b0784553cde1ba25775a
asharonbaltazar <asharonbaltazar@outlook.com>
2022-12-04 13:34:22 -0500

    feat: replace headless.ui comps with Radix comps

--- packages/ui/src/components/Menu/index.tsx
+++ packages/ui/src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { Menu as HeadlessMenu } from '@headlessui/react';
+import * as RadixMenu from '@radix-ui/react-dropdown-menu';
 import { useRouter } from 'next/router';
 import { MouseEventHandler, ReactElement, ReactNode } from 'react';
 
@@ -6,11 +6,11 @@ type MenuProps = {
   children: ReactNode;
 };
 
-export const Menu = ({ children, ...props }: MenuProps) => {
+export const Menu = ({ children }: MenuProps) => {
   return (
-    <HeadlessMenu as="div" className="relative" {...props}>
-      {children}
-    </HeadlessMenu>
+    <RadixMenu.Root>
+      <div>{children}</div>
+    </RadixMenu.Root>
   );
 };
 
@@ -18,26 +18,19 @@ type MenuButtonProps = {
   children: ReactNode;
 };
 
-const MenuButton = ({ children, ...props }: MenuButtonProps) => {
-  return (
-    <HeadlessMenu.Button as="div" {...props}>
-      {children}
-    </HeadlessMenu.Button>
-  );
+const MenuButton = ({ children }: MenuButtonProps) => {
+  return <RadixMenu.Trigger>{children}</RadixMenu.Trigger>;
 };
 
 type MenuItemsProps = {
   children: ReactNode;
 };
 
-const MenuItems = ({ children, ...props }: MenuItemsProps) => {
+const MenuItems = ({ children }: MenuItemsProps) => {
   return (
-    <HeadlessMenu.Items
-      className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-black shadow-lg shadow-stone-200 dark:shadow-stone-700 focus:outline-none p-2 z-10"
-      {...props}
-    >
+    <RadixMenu.Content className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-black shadow-lg shadow-stone-200 dark:shadow-stone-700 focus:outline-none p-2 z-10">
       {children}
-    </HeadlessMenu.Items>
+    </RadixMenu.Content>
   );
 };
 
@@ -54,41 +47,37 @@ const MenuItem = ({ icon, href, disabled, onClick, children }: MenuItemProps) =>
 
   if (href) {
     return (
-      <HeadlessMenu.Item>
-        {({ active }) => (
-          <a
-            href={href}
-            onClick={event => {
-              event.preventDefault();
-              router.push(href);
-            }}
-            className={`${active ? 'bg-stone-100 dark:bg-stone-800' : ''} ${
-              disabled ? 'cursor-not-allowed opacity-50' : ''
-            } select-none text-sm text-stone-800 dark:text-stone-300 px-4 py-1 w-full rounded-md text-left flex gap-2 items-center`}
-          >
-            {icon}
-            {children}
-          </a>
-        )}
-      </HeadlessMenu.Item>
-    );
-  }
-
-  return (
-    <HeadlessMenu.Item>
-      {({ active }) => (
-        <button
-          type="button"
-          onClick={onClick}
+      <RadixMenu.Item asChild>
+        <a
+          href={href}
+          onClick={event => {
+            event.preventDefault();
+            router.push(href);
+          }}
           className={`${active ? 'bg-stone-100 dark:bg-stone-800' : ''} ${
             disabled ? 'cursor-not-allowed opacity-50' : ''
           } select-none text-sm text-stone-800 dark:text-stone-300 px-4 py-1 w-full rounded-md text-left flex gap-2 items-center`}
         >
           {icon}
           {children}
-        </button>
-      )}
-    </HeadlessMenu.Item>
+        </a>
+      </RadixMenu.Item>
+    );
+  }
+
+  return (
+    <RadixMenu.Item asChild>
+      <button
+        type="button"
+        onClick={onClick}
+        className={`${active ? 'bg-stone-100 dark:bg-stone-800' : ''} ${
+          disabled ? 'cursor-not-allowed opacity-50' : ''
+        } select-none text-sm text-stone-800 dark:text-stone-300 px-4 py-1 w-full rounded-md text-left flex gap-2 items-center`}
+      >
+        {icon}
+        {children}
+      </button>
+    </RadixMenu.Item>
   );
 };
 
