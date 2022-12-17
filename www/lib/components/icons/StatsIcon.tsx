0a4c3629b7aa3098c7246735a09d2a6bcd5245f7
QuiiBz <tom.lienrd@gmail.com>
2022-12-17 13:57:10 +0100

    fix: svg props

--- www/lib/components/icons/StatsIcon.tsx
+++ www/lib/components/icons/StatsIcon.tsx
@@ -3,9 +3,9 @@ export const StatsIcon = () => (
     <path
       d="M13.3333 30L13.3333 25M20 30V23.3333M26.6666 30V20M26.6666 10C25 16.6667 18.3333 20 13.3333 20M26.6666 10L21.6666 11.6667M26.6666 10L28.3333 15"
       stroke="#050211"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
