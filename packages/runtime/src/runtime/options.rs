48aff2e7d4674ca6914b3fbc946346b9bcc7d79f
QuiiBz <tom.lienrd@gmail.com>
2023-01-01 10:17:57 +0100

    fix(runtime): derive default RuntimeOptios

--- packages/runtime/src/runtime/options.rs
+++ packages/runtime/src/runtime/options.rs
@@ -1,15 +1,7 @@
+#[derive(Default)]
 pub struct RuntimeOptions {
     pub allow_code_generation: bool,
     pub expose_gc: bool,
-}
-
-impl Default for RuntimeOptions {
-    fn default() -> Self {
-        Self {
-            allow_code_generation: true,
-            expose_gc: false,
-        }
-    }
 }
 
 impl RuntimeOptions {
