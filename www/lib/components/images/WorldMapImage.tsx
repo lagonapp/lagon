e13863f4eaf6460af760fb5c78fec65e586fe546
QuiiBz <tom.lienrd@gmail.com>
2023-01-21 12:23:40 +0100

    fix: svg props

--- www/lib/components/images/WorldMapImage.tsx
+++ www/lib/components/images/WorldMapImage.tsx
@@ -1,7 +1,7 @@
 export const WorldMapImage = () => (
   <svg
-    width="auto"
-    height="auto"
+    width="100%"
+    height="100%"
     viewBox="0 0 1202 590"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
