1619b33ee0f485bdd8d23e29a37bc36f43747b37
QuiiBz <tom.lienrd@gmail.com>
2023-01-29 12:05:10 +0100

    chore: add blog

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{md,mdx}",
-    "./theme.config.tsx",
-  ],
+  content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{md,mdx}', './theme.config.tsx'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
