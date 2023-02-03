fea698ac9eac5969e4c55961bbdba73cbe9fd4af
Tom Lienard <tom.lienrd@gmail.com>
2023-02-03 19:47:39 +0100

    refactor(dashboard,serverless): remove useless LAGON_TOKEN env variable (#558)

--- packages/dashboard/lib/api/index.ts
+++ packages/dashboard/lib/api/index.ts
@@ -1,23 +1,6 @@
 import { withSentry } from '@sentry/nextjs';
 import { NextApiHandler } from 'next';
 
-type ApiHandlerOptions = {
-  tokenAuth?: boolean;
-};
-
-export default function apiHandler(
-  nextHandler: NextApiHandler,
-  options: ApiHandlerOptions = { tokenAuth: true },
-): NextApiHandler {
-  const { tokenAuth } = options;
-
-  return async (request, response) => {
-    if (tokenAuth) {
-      if (request.headers['x-lagon-token'] !== process.env.LAGON_TOKEN) {
-        return response.status(401).end();
-      }
-    }
-
-    return withSentry(nextHandler)(request, response);
-  };
+export default function apiHandler(nextHandler: NextApiHandler): NextApiHandler {
+  return async (request, response) => withSentry(nextHandler)(request, response);
 }
