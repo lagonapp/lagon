854a6d821517c35e552264fb2719e8e4a37e1c98
QuiiBz <tom.lienrd@gmail.com>
2023-02-10 18:14:24 +0100

    feat: add next-sitemap

---
+++ packages/docs/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.lagon.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
---
+++ www/next-sitemap.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lagon.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  transform: (config, path) => {
+    if (path === '/') {
+      return {
+        loc: path,
+        changefreq: 'weekly',
+        priority: 1,
+      };
+    }
+
+    if (path === '/pricing') {
+      return {
+        loc: path,
+        changefreq: 'weekly',
+        priority: 0.9,
+      };
+    }
+
+    if (path.startsWith('/blog')) {
+      return {
+        loc: path,
+        changefreq: 'daily',
+        priority: 0.8,
+      };
+    }
+
+    return {
+      loc: path,
+      priority: config.priority,
+    };
+  },
+};
