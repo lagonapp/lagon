cbedd2e2565849fb0c5de0d2b9680acb98251fc1
QuiiBz <tom.lienrd@gmail.com>
2023-02-15 10:28:10 +0100

    fix(dashboard): playground uses dom types

--- packages/dashboard/lib/components/Playground.tsx
+++ packages/dashboard/lib/components/Playground.tsx
@@ -18,60 +18,9 @@ const Playground = ({ defaultValue, width, height }: PlaygroundProps) => {
       });
 
       monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-        lib: ['es2015'],
+        lib: ['es2015', 'dom'],
         allowNonTsExtensions: true,
       });
-
-      const code = `declare interface RequestInit {
-          method?: string;
-          headers?: Record<string, string | string[] | undefined>;
-          body?: string;
-        }
-
-        declare class Request {
-          constructor(input: string, options?: RequestInit);
-
-          method: string;
-          headers: Record<string, string | string[] | undefined>;
-          body?: string;
-          url: string;
-
-          text(): string;
-          json(): object;
-          formData(): Record<string, string>;
-        }
-
-        declare interface ResponseInit {
-          status?: number;
-          statusText?: string;
-          headers?: Record<string, string | string[] | undefined>;
-          url?: string;
-        }
-
-        declare class Response {
-          constructor(body?: string, options?: ResponseInit);
-
-          body: string;
-          headers?: Record<string, string | string[] | undefined>;
-          ok: boolean;
-          status: number;
-          statusText: string;
-          url: string;
-
-          text(): string;
-          json(): object;
-          formData(): Record<string, string>;
-        }
-
-        declare function fetch(resource: string, init?: RequestInit): Promise<Response>;
-        `;
-      const file = 'ts:filename/lagon.d.ts';
-
-      monaco.languages.typescript.typescriptDefaults.addExtraLib(code, file);
-
-      if (!monaco.editor.getModel(monaco.Uri.parse(file))) {
-        monaco.editor.createModel(code, 'typescript', monaco.Uri.parse(file));
-      }
     }
   }, [monaco]);
 
