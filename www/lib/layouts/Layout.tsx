3698273e315edc832723865723fae396a55c4610
QuiiBz <tom.lienrd@gmail.com>
2023-03-06 15:01:54 +0100

    feat: remove @next/font

--- www/lib/layouts/Layout.tsx
+++ www/lib/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 import { Header } from '../components/Header';
 import useSystemTheme from '../hooks/useSystemTheme';
 import { ReactNode } from 'react';
