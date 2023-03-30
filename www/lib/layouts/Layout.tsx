aecb8da701badcbf5e114a752937290b27e72da7
QuiiBz <tom.lienrd@gmail.com>
2023-03-30 18:47:40 +0200

    refactor: remove useSystemTheme hook

--- www/lib/layouts/Layout.tsx
+++ www/lib/layouts/Layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google';
 import { Header } from '../components/Header';
-import useSystemTheme from '../hooks/useSystemTheme';
 import { ReactNode } from 'react';
 import { Footer } from '../components/Footer';
 
@@ -13,27 +12,18 @@ type LayoutProps = {
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  // const theme = useSystemTheme();
-
   return (
-    <>
-      {/* {theme === 'dark' ? (
-          <link rel="icon" href="/favicon-white.ico" />
-        ) : (
-          <link rel="icon" href="/favicon-black.ico" />
-        )} */}
-      <div className={`bg-dark pt-8 ${inter.className}`}>
-        <div
-          className="pointer-events-none absolute top-0 left-0 h-64 w-full"
-          style={{
-            background:
-              'linear-gradient(to bottom, transparent, #050211), repeating-linear-gradient(-45deg, #041F47, #041F47 1px, transparent 1px, transparent 20px)',
-          }}
-        />
-        <Header />
-        <main className="container mx-auto flex min-h-screen flex-col gap-32 px-4 pt-24 md:gap-64">{children}</main>
-        <Footer />
-      </div>
-    </>
+    <div className={`bg-dark pt-8 ${inter.className}`}>
+      <div
+        className="pointer-events-none absolute top-0 left-0 h-64 w-full"
+        style={{
+          background:
+            'linear-gradient(to bottom, transparent, #050211), repeating-linear-gradient(-45deg, #041F47, #041F47 1px, transparent 1px, transparent 20px)',
+        }}
+      />
+      <Header />
+      <main className="container mx-auto flex min-h-screen flex-col gap-32 px-4 pt-24 md:gap-64">{children}</main>
+      <Footer />
+    </div>
   );
 };
