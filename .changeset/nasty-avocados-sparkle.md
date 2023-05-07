4c8a6c6318d44586de38910c8ac561327e23d23c
QuiiBz <tom.lienrd@gmail.com>
2023-05-07 11:23:25 +0200

    feat(dashboard,docs): improve environment variables

---
+++ .changeset/nasty-avocados-sparkle.md
@@ -0,0 +1,5 @@
+---
+'@lagon/dashboard': patch
+---
+
+Fix page title on functions overview
--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -3,15 +3,17 @@ import { VariantProps } from 'class-variance-authority';
 import { FieldValidator } from 'final-form';
 import { Text } from '../';
 import { variants } from './styles';
+import { ClipboardEventHandler } from 'react';
 
 type InputProps = {
   name: string;
   type?: string;
   placeholder?: string;
   validator?: FieldValidator<string | number>;
+  onPaste?: ClipboardEventHandler<HTMLInputElement>;
 } & VariantProps<typeof variants>;
 
-export const Input = ({ name, type = 'text', placeholder, disabled, validator }: InputProps) => {
+export const Input = ({ name, type = 'text', placeholder, disabled, validator, onPaste }: InputProps) => {
   const styles = variants({ disabled });
 
   return (
@@ -25,6 +27,7 @@ export const Input = ({ name, type = 'text', placeholder, disabled, validator }:
             disabled={!!disabled}
             aria-disabled={!!disabled}
             className={styles}
+            onPaste={onPaste}
           />
           {meta.touched && meta.error ? <Text error>{meta.error}</Text> : null}
         </div>
