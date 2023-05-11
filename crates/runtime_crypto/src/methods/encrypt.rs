94a6ec1c265e8a3036629603bf9e2b6644a4ed0b
QuiiBz <tom.lienrd@gmail.com>
2023-05-11 19:08:58 +0200

    feat(runtime): support AES-CBC for `SubtleCrypto#encrypt` & `SubtleCrypto#decrypt`

--- crates/runtime_crypto/src/methods/encrypt.rs
+++ crates/runtime_crypto/src/methods/encrypt.rs
@@ -1,7 +1,10 @@
+use aes::cipher::{block_padding::Pkcs7, BlockEncryptMut, KeyIvInit};
 use aes_gcm::{aead::Aead, KeyInit, Nonce};
 use anyhow::{anyhow, Result};
 
 use crate::{Aes256Gcm, Algorithm};
+
+type Aes128CbcEnc = cbc::Encryptor<aes::Aes256>;
 
 pub fn encrypt(algorithm: Algorithm, key_value: Vec<u8>, data: Vec<u8>) -> Result<Vec<u8>> {
     match algorithm {
@@ -14,6 +17,11 @@ pub fn encrypt(algorithm: Algorithm, key_value: Vec<u8>, data: Vec<u8>) -> Resul
                 Err(_) => Err(anyhow!("Encryption failed")),
             }
         }
+        Algorithm::AesCbc(iv) => Ok(Aes128CbcEnc::new(
+            key_value.as_slice().into(),
+            iv.as_slice().into(),
+        )
+        .encrypt_padded_vec_mut::<Pkcs7>(&data)),
         _ => Err(anyhow!("Algorithm not supported")),
     }
 }
