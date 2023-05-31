12ff77821b74bffc483a216c46053e270ddde64a
Tom Lienard <tom.lienrd@gmail.com>
2023-05-31 08:39:26 +0200

    feat(js-runtime,wpt): improve conformance (#835)

---
+++ .changeset/spicy-insects-hug.md
@@ -0,0 +1,5 @@
+---
+'@lagon/wpt-runner': patch
+---
+
+Add not completed tests to results
--- crates/wpt-runner/src/main.rs
+++ crates/wpt-runner/src/main.rs
@@ -33,12 +33,16 @@ const ENCODING_TABLE: &str = r#"const encodings_table =
   }
 ]"#;
 const REQUEST_CACHE: &str = include_str!("../../../tools/wpt/fetch/api/request/request-cache.js");
+const REQUEST_ERROR: &str = include_str!("../../../tools/wpt/fetch/api/request/request-error.js");
+const REQUEST_UTILS: &str = include_str!("../../../tools/wpt/fetch/api/resources/utils.js");
 const SUBSET_TESTS: &str = include_str!("../../../tools/wpt/common/subset-tests.js");
 const DECODING_HELPERS: &str =
     include_str!("../../../tools/wpt/encoding/resources/decoding-helpers.js");
 const SUPPORT_BLOB: &str = include_str!("../../../tools/wpt/FileAPI/support/Blob.js");
 const SUPPORT_FORMDATA: &str =
     include_str!("../../../tools/wpt/FileAPI/support/send-file-formdata-helper.js");
+const STREAM_DISTURBED_UTIL: &str =
+    include_str!("../../../tools/wpt/fetch/api/response/response-stream-disturbed-util.js");
 
 static RESULT: Lazy<Mutex<(usize, usize, usize)>> = Lazy::new(|| Mutex::new((0, 0, 0)));
 static TEST_HARNESS: Lazy<String> = Lazy::new(|| {
@@ -48,28 +52,35 @@ static TEST_HARNESS: Lazy<String> = Lazy::new(|| {
         .replace("debug: false", "debug: true")
 });
 
-const SKIP_TESTS: [&str; 16] = [
-    // request
-    "request-error.any.js",         // "badRequestArgTests is not defined"
-    "request-init-stream.any.js",   // "request.body.getReader is not a function"
-    "request-consume-empty.any.js", // "Unexpected end of JSON input"
-    "request-consume.any.js",       // "Unexpected end of JSON input"
-    "request-init-priority.any.js", // "idx is not defined"
+const SKIP_TESTS: [&str; 22] = [
     // response
     "response-cancel-stream.any.js",           // "undefined"
     "response-error-from-stream.any.js",       // "Start error"
     "response-stream-with-broken-then.any.js", // "Cannot destructure property 'done' of 'undefined' as it is undefine"
+    "response-stream-disturbed-4.any.js",
+    // request
+    "request-cache-default-conditional.any.js", // fetch a python server
+    "request-cache-default.any.js",             // fetch a python server
+    "request-cache-force-cache.any.js",         // fetch a python server
+    "request-cache-no-cache.any.js",            // fetch a python server
+    "request-cache-no-store.any.js",            // fetch a python server
+    "request-cache-only-if-cached.any.js",      // fetch a python server
+    "request-cache-reload.any.js",              // fetch a python server
     // url
-    "idlharness.any.js",  // load webidl stuff, not supported
-    "url-setters.any.js", // fetch an json file, find a way to run it
+    "idlharness.any.js",            // load webidl stuff, not supported
+    "url-setters.any.js",           // fetch an json file, find a way to run it
+    "url-setters-stripping.any.js", // stripping every field is not supported
     // encoding
     "textdecoder-utf16-surrogates.any.js", // we only support utf-8
     "textencoder-utf16-surrogates.any.js", // we only support utf-8
     "iso-2022-jp-decoder.any.js",          // we only support utf-8
     "encodeInto.any.js",                   // TextEncoder#encodeInto isn't implemented yet
-    "textdecoder-fatal-single-byte.any.js", // have a random number of tests?
+    "textdecoder-fatal-single-byte.any.js", // lots of test that we don't really need
+    "unsupported-encodings.any.js",        // we only support utf-8
     // event
     "EventTarget-removeEventListener.any.js", // removeEventListener does not exists on the global object
+    // headers
+    "header-values-normalize.any.js", // XMLHttpRequest isn't supported
 ];
 
 async fn run_test(path: &Path) {
@@ -95,15 +106,19 @@ async fn run_test(path: &Path) {
     isWindow: () => false,
 }}
 globalThis.location = {{}}
+globalThis.idx = undefined;
 
 export function handler() {{
     {}
     {ENCODING_TABLE}
     {REQUEST_CACHE}
+    {REQUEST_ERROR}
+    {REQUEST_UTILS}
     {SUBSET_TESTS}
     {DECODING_HELPERS}
     {SUPPORT_BLOB}
     {SUPPORT_FORMDATA}
+    {STREAM_DISTURBED_UTIL}
     {code}
     return new Response()
 }}",
@@ -135,6 +150,8 @@ export function handler() {{
                 println!("{}", style(content).red());
             } else if content.starts_with("TEST START") {
                 RESULT.lock().unwrap().0 += 1;
+            } else {
+                // println!("{}", content);
             }
         }
     });
@@ -209,16 +226,20 @@ async fn main() {
     let result = RESULT.lock().unwrap();
     println!();
     println!(
-        "{} tests, {} passed, {} failed",
+        "{} tests, {} passed, {} failed ({} not completed)",
         result.0,
         style(result.1.to_string()).green(),
-        style(result.2.to_string()).red()
+        style(result.2.to_string()).red(),
+        result.0 - (result.1 + result.2)
     );
 
     if result.2 == 0 {
         println!(" -> 100% conformance");
     } else {
-        println!(" -> {}% conformance", (result.1 * 100) / result.0);
+        println!(
+            " -> {}% conformance",
+            (result.1 * 100) / (result.1 + result.2)
+        );
     }
 
     runtime.dispose();
