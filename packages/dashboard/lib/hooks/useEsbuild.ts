87ec5efa77a55d8200ba2da2c0cf32c911e69aae
akitaSummer <644171127@qq.com>
2023-06-03 03:37:29 +0800

    fix: fix git lint

--- packages/dashboard/lib/hooks/useEsbuild.ts
+++ packages/dashboard/lib/hooks/useEsbuild.ts
@@ -64,7 +64,7 @@ export enum ESBuildStatus {
 }
 
 class EsBuildSingleton {
-  static isFirst: boolean = true;
+  static isFirst = true;
   static getIsFirst = () => {
     if (EsBuildSingleton.isFirst) {
       EsBuildSingleton.isFirst = false;
