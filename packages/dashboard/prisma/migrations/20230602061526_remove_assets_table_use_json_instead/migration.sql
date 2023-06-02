9643de7024ebd0ac4814418b8d1d00236cf4e23e
QuiiBz <tom.lienrd@gmail.com>
2023-06-02 08:18:38 +0200

    feat(dashboard): set json default value

--- packages/dashboard/prisma/migrations/20230602061526_remove_assets_table_use_json_instead/migration.sql
+++ packages/dashboard/prisma/migrations/20230602061526_remove_assets_table_use_json_instead/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - You are about to drop the `Asset` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `assets` to the `Deployment` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
