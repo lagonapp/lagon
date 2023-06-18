fe5a96d9cdf0227fdbb3d20fd7a06ba649da67db
Tom Lienard <tom.lienrd@gmail.com>
2023-06-18 20:08:32 +0200

    chore(examples): add a more complex Nuxt example (#970)

---
+++ examples/nuxt-complex/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </section>
+</template>
---
+++ examples/nuxt-complex/pages/users/[user].vue
@@ -0,0 +1,11 @@
+<script setup>
+const route = useRoute()
+
+const { user } = route.params
+</script>
+
+<template>
+  <section>
+    <h1>This is the user route: {{ user }}</h1>
+  </section>
+</template>
