2ab52c6b4b8a5bebd2c6580e2076d12568080080
QuiiBz <tom.lienrd@gmail.com>
2023-06-25 10:13:42 +0200

    fix: prevent exit code 1

--- crates/cli/npm/install.js
+++ crates/cli/npm/install.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 
+// Prevent exiting with code 1 when
+// the changeset PR is created
+process.exit = () => {};
+
 import { getBinary } from './getBinary.js';
 getBinary().install();
