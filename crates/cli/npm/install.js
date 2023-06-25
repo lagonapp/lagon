3303da414911c1ec7ef418773e1c8d72523848a0
QuiiBz <tom.lienrd@gmail.com>
2023-06-25 09:40:34 +0200

    refactor(cli): binary install

--- crates/cli/npm/install.js
+++ crates/cli/npm/install.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 
-// Prevent exiting with code 1
-process.exit = () => {};
-
-try {
-  import('./getBinary.js').then(({ getBinary }) => {
-    getBinary().install();
-  });
-} catch (error) {
-  console.error(error);
-}
+import { getBinary } from './getBinary.js';
+getBinary().install();
